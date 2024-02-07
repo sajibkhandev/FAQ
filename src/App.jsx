53f18e20c81799aa79f0d5fc55944dc7c35e872e
sajibkhandev <sajibkhan.mern@gmail.com>
2024-02-08 05:53:35 +0600

    faq

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [condition,setCondition]=useState(false)
+  let handleClick=()=>{
+    setCondition(!condition)
+
+  }
+
+  
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-blue-200 h-screen pt-20">
+      <div className="bg-yellow-100 mx-auto w-[700px] p-5">
+        <div className="flex justify-between">
+          <h2 className="text-2xl font-semibold pb-2">What is your Name?</h2>
+          <span onClick={handleClick} className="cursor-pointer text-2xl">{condition?"+":"-"}</span>
+        </div>
+        {
+          condition&&
+        <p className="text-base ">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam itaque in reiciendis voluptatibus corporis possimus a pariatur voluptatem? Reiciendis, voluptatibus?</p>
+        }
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
+    
     </>
   )
 }
